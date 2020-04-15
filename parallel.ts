c511fe5a1ebf9e6a9dae16e8f3e1485044c8cc5b
KalbeAbbas <kalbeabbas142@gmail.com>
2020-04-15 19:35:07 +0500

    first commit

---
+++ parallel.ts
@@ -0,0 +1,10 @@
+//% color=#444444 icon="\uf0eb"
+namespace test_parallel
+{
+	//% block="run parallel %u"
+	//% shim=parall::startParallel
+	export function startParallel(u: () => void)
+	{
+	return 1;
+	}
+}
